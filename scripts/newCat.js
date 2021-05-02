7e318131efc27bf596d8988a8eabaafb10c78b26
Ron Hsu <up6nk4ru4n@gmail.com>
2021-05-02 22:50:51 +0800

    [update] add current box

--- scripts/newCat.js
+++ scripts/newCat.js
@@ -64,7 +64,7 @@ class Cat {
   	if(this.leftEyeColor != this.rightEyeColor) {
   		eyeText = "異色瞳(左" + this.leftEyeColor + "右" + this.rightEyeColor + ")";
   	}
-  	return "一隻" + this.kind + "的" + eyeText + this.bodyColor + "貓";
+  	return this.kind + "的" + eyeText + this.bodyColor + "貓";
   }
 
   getHtmlId() {
@@ -73,6 +73,45 @@ class Cat {
 
   getCode() {
   	return this.code;
+  }
+
+  static getInfoByCode(code) {
+  	var splitCode = code.split("-");
+  	// body
+  	if(!$.isNumeric(splitCode[0])){
+  		throw new Error("Invalid body code.");
+  	}
+  	var idxBodyColor = parseInt(splitCode[0]);
+    var bodyColor = catSetting.bodyColorArray[idxBodyColor];
+
+  	// kind
+  	if(!$.isNumeric(splitCode[1])){
+  		throw new Error("Invalid kind code.");
+  	}
+  	var idxKind = parseInt(splitCode[1]);
+    var kind = catSetting.kindArray[idxKind];
+
+  	// left eye
+  	if(!$.isNumeric(splitCode[2])){
+  		throw new Error("Invalid left eye code.");
+  	}
+  	var idxLeftEyeColor = parseInt(splitCode[2]);
+    var leftEyeColor = catSetting.eyeColorArray[idxLeftEyeColor];
+
+  	// right eye
+  	if(!$.isNumeric(splitCode[3])){
+  		throw new Error("Invalid right eye code.");
+  	}
+  	var idxRightEyeColor = parseInt(splitCode[3]);
+	var rightEyeColor = catSetting.eyeColorArray[idxRightEyeColor];
+
+  	var eyeText = leftEyeColor + "眼";
+  	if(leftEyeColor != rightEyeColor) {
+  		eyeText = "異色瞳(左" + leftEyeColor + "右" + rightEyeColor + ")";
+  	}
+  	var info = kind + "的" + eyeText + bodyColor + "貓";
+
+  	return info;
   }
 }
 
@@ -106,7 +145,7 @@ function showCat(targetHtmlId) {
 				"<div class='radio'>" +
 				"<label for='" + item.getHtmlId() + "'>" + 
 				"<input id='" + item.getHtmlId() + "' type='radio' name='" + Cat.$prefix + "' value='" + item.getCode() + "' />" + 
-				"<span>" + item.info + "</span>" +
+				"一隻<span>" + item.info + "</span>" +
 				"</label>" +
 				"</div>"
 			);
@@ -129,14 +168,34 @@ function switchCat(oldHtmlId, newHtmlId) {
 			var catCode = jqChecked.val();
 
 			var idx = jqOld.find(".history").length + 1;
+			var currentHtmlId = "History" + idx;
+			var htmlHistory = 
+				catText + "<input type='hidden' name='CatCode' value='" + catCode + "' />";
 			jqOld.append(
-				"<div id='History" + idx + "' class='history'>" +
-				"你的第" + idx + "隻貓咪是："　+ catText + 
-				"<input type='hidden' name='CatCode' value='" + catCode + "' />" +
+				"<div id='" + currentHtmlId + "' class='history'>" +
+				"你的第" + idx + "隻貓咪是：" +
+				htmlHistory +
 				"</div>"
 			);
-			jqOld.show();
+			if(jqOld.hasClass("hide")){
+				jqOld.removeClass("hide");
+			}
 			jqNew.empty();
+
+			// first
+			if(idx <= 1) {
+				$("#FirstCat").html("這是一隻" + htmlHistory);
+				$("#CurrentBox").removeClass("hide");
+				$("#CurrentLine").removeClass("hide");
+			} 
+			// second
+			else {
+				var htmlSecond = $("#SecondCat").html();
+				if(htmlSecond.trim().length > 0) {
+					$("#FirstCat").html(htmlSecond);
+				} 
+				$("#SecondCat").html("這是一隻" + htmlHistory);
+			}
 
 			createCat();
 			showCat(newHtmlId);
@@ -145,8 +204,6 @@ function switchCat(oldHtmlId, newHtmlId) {
 		}
 	}
 }
-
-
 
 
 
